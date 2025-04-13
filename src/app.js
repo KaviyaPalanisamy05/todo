729a1f276736bd55c97eff4f49e4a0fd7b0b5cf7
KaviyaPalanisamy05 <kavya05palani@gmail.com>
2025-04-13 15:31:49 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const todoRouter=require('./routes/todo');
 const app=express();
 const PORT=3000;
 
+app.use(express.json());//after edit in todo.js
 app.use('/todo',todoRouter);
 app.listen(PORT, () =>{
     console.log(`Server is running in port:`, PORT);
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,3 +1,13 @@
 const express=require('express');
+const { createTODO } = require('../models/todo');
 const router= express.Router();
+
+router.post('/',async(req, res)=>{
+    const {title,description} = req.body;
+    // console.log({body: req.body});  //after JSON edit in Thunder
+    const todo =await createTODO(title,description);
+    res.send({messgae : 'not implemented'});
+});
+
 module.exports=router;
+
